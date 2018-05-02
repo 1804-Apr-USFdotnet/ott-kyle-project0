73c99d37f066472e0d9ac62db5cf21a7b78cef10
kyleott <kott@worcester.edu>
2018-05-01 20:16:50 -0400

    Final

--- RestaurantReviews/ConsoleApp/Program.cs
+++ RestaurantReviews/ConsoleApp/Program.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RestaurantReviews.Model;
+using NLog;
 
 namespace ConsoleApp
 {
     public class Program
     {
+        //static Logger logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            Serial ser = new Serial();
+    
             Functions func = new Functions();
+            var logger = NLog.LogManager.GetCurrentClassLogger();
 
             while (true)
             {
@@ -26,9 +31,8 @@ Please choose from the following options:
 5. Enter a restaurant name for their average rating
 6. Show the top three restaurants based on ratings
 0. To exit");
-
+                logger.Log(LogLevel.Info, "TEST");
                 var choice = Console.ReadLine();
-                //Console.WriteLine("ksjadfb");
                 if (choice == "1")
                 {
                     func.DisplayAll();
@@ -37,24 +41,28 @@ Please choose from the following options:
                 {
                     Console.WriteLine("Enter restaurant name: ");
                     string read = Console.ReadLine();
+                    logger.Debug(read);
                     func.Details(read);
                 }
                 else if (choice == "3")
                 {
                     Console.WriteLine("Enter restaurant name: ");
                     string read = Console.ReadLine();
+                    logger.Debug(read);
                     func.AllReviews(read);
                 }
                 else if (choice == "4")
                 {
                     Console.WriteLine("Enter any characters: ");
                     string read = Console.ReadLine();
+                    logger.Debug(read);
                     func.SearchRestaurant(read);
                 }
                 else if (choice == "5")
                 {
                     Console.WriteLine("Enter restaurant name: ");
                     string read = Console.ReadLine();
+                    logger.Debug(read);
                     func.GetAverageRating(read);
                 }
                 else if (choice == "6")
---
+++ RestaurantReviews/RestaurantReviews/Classes/Serial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace RestaurantReviews.Model
+{
+    public class Serial
+    {
+        Restaurant rest = new Restaurant();
+        
+        public void main()
+        {
+            StreamWriter sw = new StreamWriter(@"C:\revature\ott-kyle-project0\RestaurantReviews\Serialization.txt");
+            rest.ID = 5;
+            rest.Name = "IHOP";
+            rest.Address = "34543 Main St";
+            rest.AverageRating = 4;
+
+            System.Xml.Serialization.XmlSerializer x = new System.Xml.Serialization.XmlSerializer(rest.GetType());
+            x.Serialize(sw, rest);
+            sw.Close();
+            Restaurant restOut = (Restaurant)x.Deserialize(new StreamReader(@"C:\revature\ott-kyle-project0\RestaurantReviews\Serialization.txt"));
+            Console.WriteLine(restOut.Name);
+        }
+    }
+}
